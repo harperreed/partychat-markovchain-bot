ae21fed96582b0bdfdabef52f84657d350b4b5a2
Harper Reed <harper@nata2.org>
2010-07-07 16:47:50 -0500

    ok. it 'works' let's see how it goes. fixed some basica bugs and what not.

--- markov_chat.py
+++ markov_chat.py
@@ -5,35 +5,44 @@ import tornado.ioloop
 import tornado.web
 
 import logging
+import urllib, urllib2
 
 
 markovchain = Markov()
+command = '/markov'
+training_text = 'chat.log'
+webhook_url = 'http://partychat-hooks.appspot.com/post/p_zjrhnzm1'
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        chattiness = self.get_augument('chattiness',1)
         names = markovchain.generate_simple_sentence()
         self.write(names)
     def post(self):
         message_body = self.get_argument('body')
-        self.write(message_body)
         message_parts = message_body.split("] ")
         user = message_parts[0].replace("[",'')
         message = message_parts[1]
-        training_file.write(message+"\n")
-        markovchain.load_brain(message)
-        
-        print message_parts
-        print user
-        print message
+        if command in message:
+            #chattiness = self.get_augument('chattiness',1)
+            sentence = markovchain.generate_simple_sentence()
+            print sentence
+            values = {'message' :sentence }
+            data = urllib.urlencode(values)
+            req = urllib2.Request(webhook_url+"?message="+str(urllib.quote(sentence)))
+            response = urllib2.urlopen(req)
+            print response
+            response.close()
 
-        pass
+        else:
+            training_file.write(message+"\n")
+            markovchain.load_brain(message)
 
 application = tornado.web.Application([
     (r"/", MainHandler),
 ])
 
 if __name__ == "__main__":
-    training_text = 'chat.log'
     training_file = open(training_text, 'r+')
     print "Loading "+training_text+" into brain."
     markovchain.load_brain(training_file)
