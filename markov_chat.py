e6ee1b5951510fe3f8ab54e096e4cf01681302ce
Harper Reed <harper@nata2.org>
2010-07-07 15:54:18 -0500

    debugging set for post

--- markov_chat.py
+++ markov_chat.py
@@ -4,33 +4,36 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
+import logging
+
 
 markovchain = Markov()
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        alias = self.get_argument('alias')
         names = markovchain.generate_simple_sentence(number_names)
         n = "<br />".join(names)
+        logging.warning('asd')
         self.write(n)
+    def post(self):
+        message_body = self.get_argument('body')
+        self.write(message_body)
+        print message_body
+
+        pass
 
 application = tornado.web.Application([
     (r"/", MainHandler),
 ])
 
 if __name__ == "__main__":
-    #Usage: python baby_chains.py <length>
-    if len(sys.argv) == 2:
-        number_names = int(sys.argv[1])
-    else:
-        number_names = 30
-
     training_text = 'names.lst'
     f = open(training_text, 'r')
     print "Loading "+training_text+" into brain."
     markovchain.load_brain(f)
     print 'Brain Reloaded'
     f.close()
-    print "Outputting " + str(number_names) + " generated names"
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
